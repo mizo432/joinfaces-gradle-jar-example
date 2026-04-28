edb199f177126b2c9bcb2fe1b883811596b881e2
Mizoguchi <hachirouuemon.mizoguhi@gmail.com>
2026-04-28 11:07:00 +0900

    Refactor Logger import for readability in `DummyServiceImpl`.

--- src/main/java/org/joinfaces/example/dummy/command/DummyServiceImpl.java
+++ src/main/java/org/joinfaces/example/dummy/command/DummyServiceImpl.java
@@ -2,11 +2,12 @@ package org.joinfaces.example.dummy.command;
 
 import java.time.LocalDateTime;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DummyServiceImpl implements DummyService{
-private static final Logger log = org.slf4j.LoggerFactory.getLogger(DummyServiceImpl.class);
+private static final Logger log = LoggerFactory.getLogger(DummyServiceImpl.class);
 
   @Override
   public String getText() {
